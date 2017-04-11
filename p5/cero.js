93bd18f0ad8c70db4e75b817cbf6fc3cfd79af60
jzumaranl <jzumaranl@gmail.com>
2017-04-10 22:50:56 -0300

    Update cero.js

--- p5/cero.js
+++ p5/cero.js
@@ -1,12 +1,17 @@
 function setup() {
-  createCanvas(300, 300);
-  background(126,42,81);
+  createCanvas(windowWidth/2,windowHeight/2);
+background(color(0, 0, 255));
+noLoop();
 }
 
 function draw() {
-  rectMode(CENTER);
-  rect(width/2, height/2, 50, 50);
-  triangle(30, 75, 58, 20, 86, 75);
-  triangle(30, 75, 58, 120, 86, 75);
-  line(mouseX, 0, mouseX, 100);
+  background(200);
+//  ellipse(width/2, height/2, 50, 50);
+//line(30, 20, 85, 75);
+//triangle(30, 75, 60, 20, 90, 75);
+  for (var x = 0; x < windowWidth/2; x+=10) {
+    for (var y = 0; y < windowHeight/2; y+=10) {
+  rect(x,y,5,5);
+  }
+ }
 }
